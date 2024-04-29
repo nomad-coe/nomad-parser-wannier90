5175225f244dc00718d56165a7d423bbfdc45c9c
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-04-29 14:57:46 +0200

    Initial commit

---
+++ src/nomad_parser_wannier90/__init__.py
@@ -0,0 +1,1 @@
+from .parser import Wannier90Parser
--- src/nomad_parser_wannier90/__main__.py
+++ src/nomad_parser_wannier90/__main__.py
@@ -1,7 +1,8 @@
 #
 # Copyright The NOMAD Authors.
 #
-# This file is part of NOMAD. See https://nomad-lab.eu for further info.
+# This file is part of NOMAD.
+# See https://nomad-lab.eu for further info.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,29 +16,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import sys
+import json
+import logging
 
-import pytest
-import numpy as np
-import os
-
+from nomad.utils import configure_logging
 from nomad.datamodel import EntryArchive
 from nomad_parser_wannier90.parser import Wannier90Parser
 
-
-def approx(value, abs=0, rel=1e-6):
-    return pytest.approx(value, abs=abs, rel=rel)
-
-
-@pytest.fixture(scope='module')
-def parser():
-    return Wannier90Parser()
-
-
-def test_single_point_La2CuO4(parser):
+if __name__ == '__main__':
+    configure_logging(console_log_level=logging.DEBUG)
     archive = EntryArchive()
-    parser.parse(
-        os.path.join(os.path.dirname(__file__), 'data/lco_mlwf/lco.wout'),
-        archive,
-        None,
-    )
-    sec_run = archive.run[-1]
+    Wannier90Parser().parse(sys.argv[1], archive, logging)
+    json.dump(archive.m_to_dict(), sys.stdout, indent=2)
